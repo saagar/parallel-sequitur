bfeb3e92367b25b0fc205a2b04ef8aa6f7b79d12
Irene Chen <ichen@resonance.seas.harvard.edu>
2012-11-30 20:16:54 -0500

    irene update nonterminal

--- nonTerminal.py
+++ nonTerminal.py
@@ -4,7 +4,7 @@ class nonTerminal:
     based on java implementation
     """
 
-    rule r;
+    rule r
 
     def __init__(self, theRule):
         r = theRule
@@ -13,4 +13,31 @@ class nonTerminal:
         p = null
         n = null
 
-    
+    class clone:
+        sym = nonTerminal(self.r)
+        sym.p = p
+        sym.n = n
+        return sym
+
+    def cleanUp():
+        join(p,n)
+        deleteDigram()
+        self.r.count -= 1
+
+    def isNonTerminal():
+        return true
+
+    """
+    this symbol is the last reference to its rule.
+    the contents of the rule are substituted in
+    its place.
+    """
+
+    def expand():
+        join(p,r.first())
+        join(r.last(),n)
+
+        theDigrams.put(r.last(), r.last())
+
+        r.theGuard.r = null
+        r.theGuard = null
