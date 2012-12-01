3dcd9cbe45de923fbb519b16e78773efcee5467e
Saagar Deshpande <saagar@resonance.seas.harvard.edu>
2012-11-30 20:04:10 -0500

    finished symbol, need to review code

--- symbol.py
+++ symbol.py
@@ -12,7 +12,7 @@ class Symbol:
     self.n = None
 
   def join(self,left, right):
-    if(left.n != None)
+    if(left.n != None):
       left.deleteDigram()
     left.n = right
     right.p = left
@@ -24,13 +24,13 @@ class Symbol:
   def cleanUp(self):
     pass
 
-  # might be fixed
+  # might be fixed - should check this
   def deleteDigram(self):
     if(self.n.isGuard()):
       return
     
     dummy = theDigrams.get(self, None)
-    if(dummy == self)
+    if(dummy == self):
       theDigrams.pop(self)
 
   def isGuard(self):
@@ -42,15 +42,64 @@ class Symbol:
   # might be fixed? may want to use get
   def check(self):
     found = None
-    if(self.n.isGuard())
+    if(self.n.isGuard()):
       return False
     if(not self in self.theDigrams):
       found = self.theDigrams[self] = self
       return False
     found = self.theDigrams[self]
-    if found.n != self
+    if found.n != selfi:
       match(self, found)
     return True
 
+  # should check this
+  def substitute(self, rule):
+    cleanUp
+    self.n.cleanUp
+    r = nonTerminal(rule)
+    self.p.insertAfter(r)
+    if(not self.p.check()):
+      self.p.n.check()
+
+  # should check this
+  def match(self, newDigram, matchingSymbol):
+    r, first, second, dummy = None, None, None, None
+    if matchingSymbol.p.isGuard() and matchingSymbol.n.n.isGuard():
+      # reuse existing rule
+      r = ((guard)matchingSymbol.p).r
+      newDigram.substitute(r)
+    else:
+      # create a new rule
+      r = rule()
+      try:
+        first = (symbol)newDigram.clone()
+        second = (symbol)newDigram.n.clone()
+        r.theGuard.n = first
+        first.p = r.theGuard
+        first.n = second
+        second.p = first
+        second.n = r.TheGuard
+        r.theGuard.p = second
+
+        dummy = (symbol)theDigrams[first] = first
+        matchingSymbol.substitute(r)
+        newDigram.substitute(r)
+      except:
+        print "Something broke in match: ", sys.exc_info()[0]
+        raise
+
+    # check for underused rule
+    if r.first().isNonTerminal() and (nonTerminal)r.first()).r.count == 1:
+      ((nonTerminal)r.first()).expand()
+
+  def hashCode(self)
+    code = ((21599*self.value) + (20507*self.n.value))
+    code = code % self.prime
+    return code
+
+  # Test if two digrams are equal
+  # Do NOT use to compare two symbols
+  def equals(obj):
+    return ((self.value == ((symbol)obj).value) and (self.n.value == ((symbol)obj).n.value))
 
 
