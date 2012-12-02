de6791deee0ec45cd117e241dd75ba69bb26a05f
Irene Chen <ichen@resonance.seas.harvard.edu>
2012-12-01 22:26:51 -0500

    finished sequitur??

--- serial/sequitur.py
+++ serial/sequitur.py
@@ -1,14 +1,17 @@
 import rule as Rule
 
 class Sequitur:
-    def runSequitur(self):
+    def __init__(self):
         firstRule = Rule()
         i = 0
         // reset number of rules and hashtable
         rule.numRules = 0
         symbol.theDigrams.clear()
-        firstRule.last().insertAfter()
-
-    
-
+        // loop until done
+        done = False
+        input_string = "abracadabraarbadacarba"
+        for i in input_string:
+            firstRule.last().insertAfter(Terminal(i))
+            firstRule.last().p.check()
+        print firstRule.getRules()
             
