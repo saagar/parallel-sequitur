7ce81214b86fa48814efdf51f4fc24e02c5a557a
raysaagar <raysaagar@gmail.com>
2012-12-13 20:27:32 -0500

    started web pages

---
+++ web/program.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Parallel Sequitur</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+  </head>
+
+  <body>
+
+	<a href="https://github.com/raysaagar/parallel-sequitur"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+  
+    <div class="container-narrow">
+      <div class="masthead">
+		<ul class="nav nav-pills pull-left">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Problem</a></li>
+          <li class="active"><a href="program.php">Program</a></li>
+		  <li><a href="#">Performance</a></li>
+		  <li><a href="#">Results</a></li>
+		  <li><a href="#">Remarks</a></li>
+		  <li><a href="#">Extensions</a></li>
+		  <li><a href="#">Resources</a></li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div class="jumbotron">
+        <h2 class="muted">Parallel Sequitur</h2>
+        <h2 class="text-info">The Code</h2>
+        
+      </div>
+
+      <hr>
+
+		<div id="container">
+			
+			<h3> The Approach </h3>
+				
+
+			<div class="span8">
+				[TEXT GOES HERE]
+			</div>
+			<br>
+			
+			<h3> The Serial Method </h3>
+	
+			<div class="span8">
+				[TEXT GOES HERE]
+			</div>
+			<br>
+			
+			<h3> Parallel Approach 1: Merge and Replace </h3>
+	
+			<div class="span8">
+				[TEXT GOES HERE]
+			</div>
+			<br>
+			
+			<h3> Parallel Approach 2: Frequency Analysis </h3>
+	
+			<div class="span8">
+				<p>
+					In theory, Sequitur generates rules that should be applied many times in the text. If a rule is generated, then the substring contained by the rule should be found frequently in the text.	This suggested that if we can treat the 
+				</p>
+			</div>
+			<br>
+			
+	
+		</div>
+	  
+
+
+      <hr>
+
+      <div class="footer">
+		<p>
+			A Computer Science 205 Final Project by Irene Chen and Saagar Deshpande</br>
+			Harvard University
+		</p>		
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
